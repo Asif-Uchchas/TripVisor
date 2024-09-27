0ee53c4b3505c1d99d01b9eb6ad7778fccc98e21
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-09-27 12:48:42 +0600

    Add project files.

---
+++ TripVisor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TripVisor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
